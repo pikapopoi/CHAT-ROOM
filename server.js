b1612419df16c04dcfa63766217e02cb41bba7b4
pikapopoi <haarshrathi1@gmail.com>
2024-08-30 23:44:55 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
-const signaling = require('./signaling');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = socketIo(server);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
-wss.on('connection', signaling);
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('chatMessage', (data) => {
+        io.emit('chatMessage', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
